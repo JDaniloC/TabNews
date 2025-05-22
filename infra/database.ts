4c5ee41488df6569baa127df6bd2c6feba33a1d2
José Danilo Silva <49529180+JDaniloC@users.noreply.github.com>
2025-05-22 03:24:03 +0000

    :sparkles: feat: Add SSL config for connect to database

--- infra/database.ts
+++ infra/database.ts
@@ -7,16 +7,27 @@ async function query(queryOptions: { text: string; values?: any[] }) {
     password: process.env.DATABASE_PASSWORD,
     host: process.env.DATABASE_HOST || "localhost",
     port: parseInt(process.env.DATABASE_PORT || "5432", 10),
+    ssl: getSSLConfig(),
   });
-  await client.connect();
 
   try {
+    await client.connect();
     return await client.query(queryOptions);
   } catch (error) {
     console.error("Database query error:", error);
   } finally {
     await client.end();
   }
+}
+
+function getSSLConfig() {
+  if (process.env.DATABASE_CA) {
+    return {
+      ca: process.env.DATABASE_CA,
+    };
+  }
+
+  return process.env.NODE_ENV !== "development"
 }
 
 export default {
